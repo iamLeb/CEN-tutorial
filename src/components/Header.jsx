770a7637991d1c25768a59669780c64e56a7df89
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-14 09:44:54 -0500

    compiled Faiza, Dnuga, Kismat components updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import logo from '../assets/react.svg';
 const Header = () => {
     return (
-        <div>
-            This is the Header Component
-        </div>
+        <header className={'bg-neutral-800 p-5 text-white'}>
+            <div className={'flex items-center space-x-2'}>
+                <img src={logo}/>
+                <h3 className={'text-3xl font-extrabold'}>LOGO</h3>
+            </div>
+
+            <div>
+                
+            </div>
+        </header>
     );
 };
 
