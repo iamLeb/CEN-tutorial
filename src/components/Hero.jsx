6a3d2c49f83d74e943fa50458de0e47444097971
Faiza <salami.faiza15@gmail.com>
2024-06-13 22:41:03 -0500

    done with the hero aspect of the page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import clairo from '../assets/img.png'
+
+const Hero = () => {
+    return (
+        <div className={"bg-[#D1FAFF] block sm:flex flex-row"}>
+
+            <div className={"flex flex-col justify-center items-center w-full sm:w-1/2 space-y-4"}>
+                <p className={"text-4xl font-bold text-gray-800"}>Welcome</p>
+                <p className={"text-lg font-medium text-gray-600 italic"}>
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
+                    "I think we could do it if we tried, if only to say you’re mine" - Clairo
+                </p>
+            </div>
+
+            <div className={"flex justify-end w-1/2"}>
+                <img className={"hidden sm:block w-full md:w-1/2 rounded-lg"} src={clairo} alt="Clairo"/>
+            </div>
+
+        </div>
+    );
+}
+
+export default Hero;
