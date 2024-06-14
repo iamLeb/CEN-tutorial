ba46d35701e680ad6fd9e05c5bfb5e323196d5fb
Faiza <salami.faiza15@gmail.com>
2024-06-14 11:19:13 -0500

    hero part

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
+import Button from "./components/button.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
       <>
           <Header />
+          <Hero/>
+          <Button/>
+          <Footer/>
       </>
   )
 }
