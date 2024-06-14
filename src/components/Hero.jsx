ba1a7744a2987588b5f648dd94f85293da3bbe26
Faiza <salami.faiza15@gmail.com>
2024-06-14 11:43:24 -0500

    added borders

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import clairo from '../assets/img.png'
 
 const Hero = () => {
     return (
-        <div className={"bg-[#D1FAFF] block sm:flex flex-row"}>
+        <div className={"bg-[#D1FAFF] block sm:flex flex-row border-2 border-black"}>
 
             <div className={"flex flex-col justify-center items-center w-full sm:w-1/2 space-y-4"}>
                 <p className={"text-4xl font-bold text-gray-800"}>Welcome</p>
@@ -13,7 +13,7 @@ const Hero = () => {
             </div>
 
             <div className={"flex justify-end w-1/2"}>
-                <img className={"hidden sm:block w-full md:w-1/2 rounded-lg"} src={clairo} alt="Clairo"/>
+                <img className={"hidden sm:block w-full "} src={clairo} alt="Clairo"/>
             </div>
 
         </div>
