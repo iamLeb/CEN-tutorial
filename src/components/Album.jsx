68f8838fe85b5a48a24bf05cca187844cdc92316
EyambaV <valour.francis@gmail.com>
2024-06-14 15:26:24 -0500

    fixed images

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Album1 from '../assets/pic1.webp'
+import Album1 from '../assets/diary001.jpg'
 import Album2 from '../assets/immunity.png'
 import Album3 from '../assets/prettygirl.jpeg'
 import Album4 from '../assets/charm.png'
@@ -8,31 +8,30 @@ import Album4 from '../assets/charm.png'
 
 const Album = () => {
     return (
-        <div className='flex flex-col justify-center items-center sm:flex-row sm:justify-evenly bg-white shadow-lg'>
-            <div className=' w-1/6 h-48  flex justify-center align-middle group'>
-                <img className='rounded-lg absolute scale-90 object-cover w-50 h-48 group-hover:opacity-50 group-hover:scale-100' src={Album1} href='' alt=''/>
+        <div className='flex flex-col justify-evenly items-center  sm:flex-row sm:justify-evenly sm:items-middle gap-x-10 bg-white shadow-lg'>
+            <div className='h-96 flex justify-center items-center group'>
+                <img className='rounded-lg absolute scale-90 object-cover w-100 h-86 group-hover:opacity-50 group-hover:scale-100' src={Album1} alt=''/>
                 <div className="relative p-5">
                     <div className="mt-40">
-                            {/* Hidden content  **/}
-
+                            {/* Hidden content */}
                             <div
                                 className="transition-all transform 
                                 translate-y-8 opacity-0 
                                 group-hover:opacity-100 
-                                group-hover:-translate-y-12"
+                                group-hover:translate-y-15"
                             >
                                 <div className="">
-                                    <a href='https://open.spotify.com/album/3NEDQmYF1roygFssVEwkwv' className="text-lg text-white">
-                                        diary 001
+                                    <a href='https://open.spotify.com/prerelease/1hk5UEw2cPHm4ceZw3g5o5' className="text-lg text-white">
+                                        Charm
                                     </a>
                                 </div>
                             </div>
                             {/* End of hidden content */}
                     </div>
                 </div>
-            </div>
-            <div className=' w-1/6 h-48  flex justify-center align-middle group'>
-                <img className='rounded-lg absolute scale-90 object-cover w-50 h-48 group-hover:opacity-50 group-hover:scale-100' src={Album2} href='' alt=''/>
+            </div>  
+            <div className=' flex justify-center items-center group'>
+                <img className='rounded-lg absolute scale-90 object-cover w-100 h-86 group-hover:opacity-50 group-hover:scale-100' src={Album2} href='' alt=''/>
                 <div className="relative p-5">
                     <div className="mt-40">
                             {/* Hidden content  **/}
@@ -41,7 +40,7 @@ const Album = () => {
                                 className="transition-all transform 
                                 translate-y-8 opacity-0 
                                 group-hover:opacity-100 
-                                group-hover:-translate-y-12"
+                                group-hover:translate-y-15"
                             >
                                 <div className="">
                                     <a href='https://open.spotify.com/album/3NEDQmYF1roygFssVEwkwv' className="text-lg text-white">
@@ -53,28 +52,8 @@ const Album = () => {
                     </div>
                 </div>
             </div>
-            <div className=' w-1/6 h-48  flex justify-center align-middle group'>
-                <img className='rounded-lg absolute  scale-90 object-cover w-50 h-48 group-hover:opacity-50 group-hover:scale-100' src={Album3} alt=''/>
-                <div className="relative p-5">
-                    <div className="mt-40">
-                            <div
-                                className="transition-all transform 
-                                translate-y-8 opacity-0 
-                                group-hover:opacity-100 
-                                group-hover:-translate-y-12"
-                            >
-                                <div className="">
-                                    <a href='https://open.spotify.com/track/0KyAGiNGUytG5JLxJu4F6l' className="text-lg text-white">
-                                        Pretty Girl
-                                    </a>
-                                </div>
-                            </div>
-                            {/* End of hidden content */}
-                    </div>
-                </div>
-            </div>
-            <div className=' w-1/6 h-48  flex justify-center align-middle group'>
-                <img className='rounded-lg absolute scale-90 object-cover w-50 h-48 group-hover:opacity-50 group-hover:scale-100' src={Album4} alt=''/>
+            <div className=' flex justify-center items-center group'>
+                <img className='rounded-lg absolute scale-90 object-cover w-100 h-86 group-hover:opacity-50 group-hover:scale-100' src={Album3} alt=''/>
                 <div className="relative p-5">
                     <div className="mt-40">
                             {/* Hidden content */}
@@ -82,7 +61,28 @@ const Album = () => {
                                 className="transition-all transform 
                                 translate-y-8 opacity-0 
                                 group-hover:opacity-100 
-                                group-hover:-translate-y-12"
+                                group-hover:translate-y-15"
+                            >
+                                <div className="">
+                                    <a href='https://open.spotify.com/prerelease/1hk5UEw2cPHm4ceZw3g5o5' className="text-lg text-white">
+                                        Pretty Girl
+                                    </a>
+                                </div>
+                            </div>
+                            {/* End of hidden content */}
+                    </div>
+                </div>
+            </div>  
+            <div className=' flex justify-center items-center group'>
+                <img className='rounded-lg absolute scale-90 object-cover w-100 h-86 group-hover:opacity-50 group-hover:scale-100' src={Album4} alt=''/>
+                <div className="relative p-5">
+                    <div className="mt-40">
+                            {/* Hidden content */}
+                            <div
+                                className="transition-all transform 
+                                translate-y-8 opacity-0 
+                                group-hover:opacity-100 
+                                group-hover:translate-y-15"
                             >
                                 <div className="">
                                     <a href='https://open.spotify.com/prerelease/1hk5UEw2cPHm4ceZw3g5o5' className="text-lg text-white">
