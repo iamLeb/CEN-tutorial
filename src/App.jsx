1e5b4e5b3a155463ab8f1b2ac709147256813b7b
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-14 13:54:08 -0500

    fixed button to upper case

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import AdsMusic from "./components/AdsMusic.jsx";
-import Button from "./components/button.jsx";
+import Button from "./components/Button.jsx";
 import Footer from "./components/Footer.jsx";
 import Album from "./components/Album.jsx";
 
