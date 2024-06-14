e7edf4b632b620c562105802fc8cf8669ac14747
Faiza <salami.faiza15@gmail.com>
2024-06-14 11:35:23 -0500

    made changes to text padding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
 
             <div className={"flex flex-col justify-center items-center w-full sm:w-1/2 space-y-4"}>
                 <p className={"text-4xl font-bold text-gray-800"}>Welcome</p>
-                <p className={"text-lg font-medium text-gray-600 italic"}>
+                <p className={"text-lg font-medium text-gray-600 italic px-10"}>
                     {/* eslint-disable-next-line react/no-unescaped-entities */}
                     "I think we could do it if we tried, if only to say you’re mine" - Clairo
                 </p>
