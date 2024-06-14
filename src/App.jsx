100eec6f117efe5191c86d2d9c74077ad37365bd
EyambaV <valour.francis@gmail.com>
2024-06-14 13:19:16 -0500

    added photos and links

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import Button from "./components/button.jsx";
 import Footer from "./components/Footer.jsx";
+import Album from "./components/Album.jsx";
 
 function App() {
   return (
       <>
           <Header />
           <Hero />
+          <Album />
           <Button />
           <Footer />
       </>
