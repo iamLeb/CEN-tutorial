b5ef72c5b4c81e48c1f662671d59547ec7d0f0a1
dnuga <dnuga23@gmail.com>
2024-06-14 15:53:47 -0500

    Update Subscribe.jsx
    made the buttons and form touch

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -6,9 +6,9 @@ const Subscribe= () => {
                     <div>Subscribe To Our Newsletter</div>
                 </div>
 
-                <div class={"flex justify-center items-center"}>
-                    <input className={"border-2 border-black py-2 px-3 text-grey-darkest flex m-4 rounded-l-lg"} type="text" placeholder="email"></input>
-                    <button class={"hover:bg-[#a7cdf5] bg-local flex m-4 text-black font-light font p-3 rounded-r-lg border-2 border-black"}>
+                <div class={"flex justify-center items-center py-5"}>
+                    <input className={"outline-none bg-gray-200 py-2 px-3 text-white flex rounded-l-lg"} type="text" placeholder="email"></input>
+                    <button class={"flex items-center space-x-4 bg-blue-500 rounded-r-lg px-6 py-2 transition-all duration-300  hover:scale-105 font-light text-white"}>
                        Click to Subscribe
                     </button>
                 </div>
